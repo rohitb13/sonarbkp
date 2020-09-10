b8e02ca38c23f9bf3ff7182b9b3b4366c25d9a68
Rohit Bhosale <rohitb13@kpit.com>
2020-09-10 11:53:13 +0530

    First Commit

---
+++ Python Learning Material/practice/demo.py
@@ -0,0 +1,1 @@
+print("Hey Hi How are you ?")
---
+++ Python Learning Material/practice/test.py
@@ -0,0 +1,5 @@
+for num in range(6):
+    for i in range(num):
+        print (num, end=" ") #print number
+    # new line after each row to display pattern correctly
+    print("\n")
